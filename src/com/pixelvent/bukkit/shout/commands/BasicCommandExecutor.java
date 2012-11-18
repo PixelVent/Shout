f15813f83abaf54acbcd1ca0d08369ab18ca909d
Benjamin Sannholm <benjamin@sannholm.se>
2012-11-18 13:28:05 +0100

    Renamed default CommandExecutor file

--- src/com/pixelvent/bukkit/shout/commands/BasicCommandExecutor.java
+++ src/com/pixelvent/bukkit/shout/commands/BasicCommandExecutor.java
@@ -1,11 +1,11 @@
-package com.pixelvent.bukkit.shout;
+package com.pixelvent.bukkit.shout.commands;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
-public class CustomCommandExecutor implements CommandExecutor
+public class BasicCommandExecutor implements CommandExecutor
 {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
 	{
